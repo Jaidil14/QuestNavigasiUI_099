fe9b2585617f0e5db951408623acf09edfb6ebba
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-06 12:13:59 +0700

    Membuat navhost untuk menampung semua halaman yang akan dinavigasi

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -3,6 +3,7 @@ package com.example.pertemuan6
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -17,6 +18,19 @@ fun DataApp(
     modifier: Modifier
 ){
     Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.FormulirKu.name,
 
+            modifier = Modifier.padding( isiRuang)) {
+            composable(route = Navigasi.FormulirKu.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map { id -> konteks.resource.getString
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
     }
 }
