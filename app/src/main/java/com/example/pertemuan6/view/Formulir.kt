c218cb09374663bb330dfdbd55133674f29387c3
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-06 13:11:35 +0700

    Belum last commit ternyata,menyesuaikan isi file formulir, menambahkan isi fungsi FormIsian dan dependensi yg diperlukan

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -1,8 +1,16 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.pertemuan6.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun FormIsian(OnSubmitBtnClick: () -> Unit) {
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
 
 }
