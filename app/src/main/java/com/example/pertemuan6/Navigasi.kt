26eb4da0485312658c847be840597eca3dddb252
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-06 12:19:58 +0700

    Menambahkan file Formulir.kt di folder view, yg berisi fungsi FormIsian, kemudian diimport ke file Navigasi.kt sebagai fungsi dependensi

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan6.view.FormIsian
 
 enum class Navigasi {
     FormulirKu,
