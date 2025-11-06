5246af5c4c1dbd65b2f8f26e491719fb6cb6803b
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-06 12:08:03 +0700

    Menambahkan kelas dan fungsi yang akan digunakan

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -1,2 +1,22 @@
 package com.example.pertemuan6
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    FormulirKu,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+
+    }
+}
