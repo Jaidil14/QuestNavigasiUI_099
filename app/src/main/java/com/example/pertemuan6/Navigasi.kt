edbc435846c1883268f882d5ef3fef3fdbf36eec
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-06 12:16:46 +0700

    Menambahkan dependensi yang diperlukan

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -1,10 +1,13 @@
 package com.example.pertemuan6
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
